46321d597fe14c9036cb25485e3daa053a4e631f
QuangThinhTran <tranlequangthinh@gmail.com>
2024-08-06 19:44:17 +0700

    Configure project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DiaryModule } from './diary/diary.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, DiaryModule],
   controllers: [AppController],
   providers: [AppService],
 })
