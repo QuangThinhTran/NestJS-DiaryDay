19df3e1f0c9c8f0a9a54f4ba1a75e509a1e10a41
QuangThinhTran <tranlequangthinh@gmail.com>
2024-08-06 21:20:38 +0700

    Configure db MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DiaryModule } from './diary/diary.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'node:process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, DiaryModule],
+  imports: [
+    AuthModule,
+    DiaryModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.DB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
